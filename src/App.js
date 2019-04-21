1e7c1357b810e0513f212dd571f40ce9ea4fd910
Maximo Bautista <max.bautista97@gmail.com>
2019-04-21 21:48:09 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const mapStateToProps = (state) => {
 };
 
 const mapDisptachToProps = dispatch => {
-  return {  }
+  return { addItem: () =>  }
 }
 
 export default connect(mapStateToProps)(App);
